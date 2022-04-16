badb9617dc33a17b0c74ff734e866fd538f69b70
Vismay-dev <vismaysuramwar@gmail.com>
2022-04-16 12:08:08 +0400

    Works

--- client/src/Components/Modals/CreateModal.js
+++ client/src/Components/Modals/CreateModal.js
@@ -65,6 +65,32 @@ const CreateProjectModal = (props) => {
       
     }
 
+
+useEffect(
+  () => {
+    const listener = (event) => {
+      // Do nothing if clicking ref's element or descendent elements
+      if (!myRef.current || myRef.current.contains(event.target)) {
+        return;
+      }
+      props.close();
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  },
+  // Add ref and handler to effect dependencies
+  // It's worth noting that because the passed-in handler is a new ...
+  // ... function on every render that will cause this effect ...
+  // ... callback/cleanup to run every render. It's not a big deal ...
+  // ... but to optimize you can wrap handler in useCallback before ...
+  // ... passing it into this hook.
+  [myRef, () => props.close()]
+);
+
     const removeProjPic = () => {
       setImage('')
       console.log(image)
