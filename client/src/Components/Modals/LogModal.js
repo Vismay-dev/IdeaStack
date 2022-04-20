48656f040a1517bb674c84adcd38ef1416b105d7
Vismay-dev <vismaysuramwar@gmail.com>
2022-04-20 16:55:07 +0400

    make it better

--- client/src/Components/Modals/LogModal.js
+++ client/src/Components/Modals/LogModal.js
@@ -392,11 +392,11 @@ passReset?
     <div class = ' w-fit relative sm:left-0 left-[7px] mx-auto block'>
 
       <div class = 'mt-2 grid grid-cols-3 gap-4 relative  sm:w-full w-[175px] mx-auto  justify-center py-1 pb-1.5 sm:px-4 '>
-      <button onClick={()=>setForgot(false)} class="mt-3 sm:col-span-1 col-span-3 group relative sm:left-0 left-[3px] sm:w-full w-[150px] mx-auto sm:right-0 right-[2px] flex justify-center py-1.5 pb-1.5 px-4 border border-transparent text-md font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+      <button onClick={()=>setForgot(false)} type = 'button' class="mt-3 sm:col-span-1 col-span-3 group relative sm:left-0 left-[3px] sm:w-full w-[150px] mx-auto sm:right-0 right-[2px] flex justify-center py-1.5 pb-1.5 px-4 border border-transparent text-md font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         
         Cancel
       </button>
-      <button onClick={checkResetCode } class="sm:mt-3 -mt-1  sm:col-span-2 col-span-3 group relative sm:left-0 left-[3px] sm:w-full w-[150px] mx-auto sm:right-0 right-[2px] flex justify-center py-1.5 pb-1.5 px-4 border border-transparent text-md font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+      <button onClick={checkResetCode } type = 'button' class="sm:mt-3 -mt-1  sm:col-span-2 col-span-3 group relative sm:left-0 left-[3px] sm:w-full w-[150px] mx-auto sm:right-0 right-[2px] flex justify-center py-1.5 pb-1.5 px-4 border border-transparent text-md font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         <span class="absolute left-0 inset-y-0 flex items-center pl-3">
           <svg class="h-5 w-5 text-blue-600 group-hover:text-blue-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
@@ -455,11 +455,11 @@ passReset?
     <div class = ' w-fit relative sm:left-0 left-[7px] mx-auto block'>
 
       <div class = 'mt-2 grid grid-cols-3 gap-4 relative  sm:w-full w-[175px] mx-auto  justify-center py-1 pb-1.5 sm:px-4 '>
-      <button onClick={()=>setForgot(false)} class="mt-3 sm:col-span-1 col-span-3 group relative sm:left-0 left-[3px] sm:w-full w-[150px] mx-auto sm:right-0 right-[2px] flex justify-center py-1.5 pb-1.5 px-4 border border-transparent text-md font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+      <button onClick={()=>setForgot(false)} type = 'button' class="mt-3 sm:col-span-1 col-span-3 group relative sm:left-0 left-[3px] sm:w-full w-[150px] mx-auto sm:right-0 right-[2px] flex justify-center py-1.5 pb-1.5 px-4 border border-transparent text-md font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         
         Cancel
       </button>
-      <button onClick={sendResetCode} class="sm:mt-3 -mt-1  sm:col-span-2 col-span-3 group relative sm:left-0 left-[3px] sm:w-full w-[150px] mx-auto sm:right-0 right-[2px] flex justify-center py-1.5 pb-1.5 px-4 border border-transparent text-md font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+      <button onClick={sendResetCode} type = 'button' class="sm:mt-3 -mt-1  sm:col-span-2 col-span-3 group relative sm:left-0 left-[3px] sm:w-full w-[150px] mx-auto sm:right-0 right-[2px] flex justify-center py-1.5 pb-1.5 px-4 border border-transparent text-md font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         <span class="absolute left-0 inset-y-0 flex items-center pl-3">
           <svg class="h-5 w-5 text-blue-600 group-hover:text-blue-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
