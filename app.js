72042614f188536efeda4bf2e375d1414fe5ae0b
Vismay-dev <vismaysuramwar@gmail.com>
2022-08-11 15:03:55 +0400

    Iteration 2

--- app.js
+++ app.js
@@ -1,98 +1,92 @@
-const express   = require('express')
-const app       = express()
-const mongoose = require('mongoose')
-const cors = require('cors')
-const RoutesAPIUser = require('./server/routes/RoutesAPIUser')
-const RoutesAPIProject = require('./server/routes/RoutesAPIProject')
-const path = require('path')
-const dotenv = require('dotenv')
-const http = require('http').createServer(app)
-const jwt = require('jsonwebtoken')
-const User = require('./server/models/studentUser')
-const cloudinary = require('cloudinary')
-const session = require('express-session')
-const MongoDBSession = require('connect-mongodb-session')(session)
-const socketIo = require('socket.io')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const RoutesAPIUser = require("./server/routes/RoutesAPIUser");
+const RoutesAPIProject = require("./server/routes/RoutesAPIProject");
+const path = require("path");
+const dotenv = require("dotenv");
+const http = require("http").createServer(app);
+const jwt = require("jsonwebtoken");
+const User = require("./server/models/studentUser");
+const cloudinary = require("cloudinary");
+const session = require("express-session");
+const MongoDBSession = require("connect-mongodb-session")(session);
+const socketIo = require("socket.io");
 
-dotenv.config()
-
+dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
-})
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
+mongoose
+  .connect(process.env.MONGODB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("- Connected to IdeaStack Database...");
+  })
+  .catch((err) => console.log(err));
 
-mongoose.connect(process.env.MONGODB,{useUnifiedTopology:true,useNewUrlParser:true },
-    ).then(()=> {
-          console.log('- Connected to IdeaStack Database...')
-      }).catch(err=> console.log(err))
+mongoose.connection.on("error", function (err) {
+  console.log(err);
+});
 
-mongoose.connection.on('error', function (err) { console.log(err) });
+app.use(express.json());
 
-app.use(express.json())
-
-
-const io = socketIo(http,
-    { 
-    cors: {
-      origin: 'http://localhost:3000',
-      credentials: true
+const io = socketIo(http, {
+  cors: {
+    origin: "https://www.ideastack.org",
+    credentials: true,
     //   :'http://localhost:3000'
-    // 'https://www.ideastack.org'
-    }
-})
+    // ''
+  },
+});
 
-app.use(cors({
-    origin:'http://localhost:3000',
-    credentials:true
+app.use(
+  cors({
+    origin: "https://www.ideastack.org",
+    credentials: true,
     // :'http://localhost:3000'
     // 'https://www.ideastack.org'
-
-}))
-
-
-
-
-
-
-io.on('connection',(socket)=>{   
-    socket.on('disconnect',(reason)=>{
-
-    })
   })
+);
 
-app.use((req,res,next)=> {
-    req.io = io;
-    next()
-})
+io.on("connection", (socket) => {
+  socket.on("disconnect", (reason) => {});
+});
 
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
+const port = process.env.PORT || 4000;
 
-
-
-const port = process.env.PORT||4000;
-
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESHSECRET,
-    resave:false,
-    saveUninitialized:false    
-}))
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.use('/api/user',RoutesAPIUser)
-app.use('/api/project',RoutesAPIProject)
+app.use("/api/user", RoutesAPIUser);
+app.use("/api/project", RoutesAPIProject);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.use("*", express.static("client/build"));
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.use('*', express.static('client/build'));     
-
-    app.get('*', (req,res)=> {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-http.listen(port,()=> {
-    console.log(`- Successfully connected to server at port ${port}`)
-})
+http.listen(port, () => {
+  console.log(`- Successfully connected to server at port ${port}`);
+});
