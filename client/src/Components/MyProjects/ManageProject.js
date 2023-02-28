1d9b6244230f0edb6a1cb0dfd509c9d48a752c72
Vismay-dev <vismaysuramwar@gmail.com>
2023-02-28 10:59:56 +0400

    UI

--- client/src/Components/MyProjects/ManageProject.js
+++ client/src/Components/MyProjects/ManageProject.js
@@ -468,7 +468,7 @@ export default function ManageProject() {
                           d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
                         />
                       </svg>{" "}
-                      <p class="inline text-lg font-semibold">
+                      <p class="inline text-sm uppercase font-semibold">
                         <span class="md:mb-0 mb-7  relative">
                           Member selected:
                         </span>
